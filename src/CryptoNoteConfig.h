9fbe8ceb8c5a2139d69775fa417536f42c2d049d
tr4nscr1pt <hosting.bih3@gmail.com>
2017-12-12 13:55:39 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -137,7 +137,7 @@ uint32_t index;
 const char* blockId;
 };
 
-const std::initializer_list<CheckpointData> CHECKPOINTS[] = {
+const CheckpointData CHECKPOINTS[] = {
    
    
   {15000, "e0d6ec7f8c0cc47a3aa14fabf424c5f4d9e443381d0b6b4b942880e7385b9693"}
