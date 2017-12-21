cf64bab4c67c9b20824eb234004fc6ff00d8cf0a
tr4nscr1pt <hosting.bih3@gmail.com>
2017-12-21 13:45:56 +0100

    Update Daemon.cpp

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -214,7 +214,7 @@ int main(int argc, char* argv[])
 
     CryptoNote::Checkpoints checkpoints(logManager);
     for (const auto& cp : CryptoNote::CHECKPOINTS) {
-      checkpoints.add_checkpoint(cp.height, cp.blockId);
+      checkpoints.add_checkpoint(cp.index, cp.blockId);
     }
 
     if (!testnet_mode) {
